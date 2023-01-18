d1ef47ad944bfa292fc0245358aeaac5bcdd46d2
willjamesrobinson <willjamesr@gmail.com>
2023-01-19 05:44:39 +1100

    extra css stuff

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import css from './Header.module.css'
 import Logo from '../../assets/logo.png'
+import {CgShoppingBag} from 'react-icons/cg'
 
 const Header = () => {
   return (
@@ -11,14 +12,20 @@ const Header = () => {
 
         <div className={css.right}>
             <div className={css.menu}>
-            menu
+                <ul className={css.menu}>
+                    <li>Collections</li>
+                    <li>Brands</li>
+                    <li>New</li>
+                    <li>Sales</li>
+                    <li>ENG</li>
+                </ul>
             
             </div>
 
 
             <input type="text" className={css.search} />
 
-            <span>PNG icon of cart</span>
+            <CgShoppingBag className={css.cart}/>
         </div>
     </div>
   )
